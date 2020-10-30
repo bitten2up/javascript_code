7ecfafc9976ff530094b4d54df04505b229b6b11
Litten 1up <55027142+stipton7960@users.noreply.github.com>
2020-10-30 10:03:54 -0500

    Add files via upload

---
+++ javascript_test_quiz.js
@@ -0,0 +1,5 @@
+alert("This is a test quiz");
+var test_quiz= prompt("first pece of code that a programer usely learns when learning a new language", "Anser here");
+if (test_quiz === "hello world"){
+    alert ("correct");
+}else {alert("incorect");}
---
+++ name.js
@@ -0,0 +1,5 @@
+alert("put your name in");
+var name = prompt("what is your name", "your name");
+if (name === "null"){
+    alert("hello non-existant person");} else{
+alert(`hello ${name}`);}
